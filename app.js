fae861fffb49ab2ae030db08980a21765f22c44b
Robert Poston <Zedomas@gmail.com>
2020-06-16 14:11:14 -0500

    added css and pulling cards for the player

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 const url = "https://deckofcardsapi.com/api/deck/"
-const newDeck = "new/shuffle/?deck_count=6"
+const newDeck = "new/shuffle/?deck_count=1"
 const draw = "/draw/?count=2"
 const shuffle = "/shuffle/"
 
-function newDeck() { 
+function newDeckMaker() { 
     let deck = $.ajax({
     type: 'GET',
     async: false,
     url: url+newDeck,
-    success: function(data) {
-        
-    } 
   });
   return deck.responseJSON.deck_id;
 }
 
+
+
 $(() => {
-    let deckID = decker()
+    function drawer() {
+        let cards = $.ajax({
+                type: 'GET',
+                async: false,
+                url: url+deckID+draw
+            })
+            return cards.responseJSON
+        }
+    
+
+
+    let deckID = newDeckMaker()
     console.log(deckID)
+   
+    function playerCards() {
+        let hand = drawer()
+        console.log(hand)
+        $('.remaining').text(hand.remaining)
+        console.log(cards)
+        $('.left').css("background-image", "url("+hand.cards[0].image+")")
+        $('.right').css("background-image", "url("+hand.cards[1].image+")")
+        
+    }
+   $('.button').on("click", playerCards)
 })
 
 
